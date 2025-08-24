b2e744c36c13511fc1edba059d4a06d085607a83
Freaky Mango Studio <office@freakymango-studio.com>
2025-08-24 11:02:31 +0100

    23.08.

--- level_1.gd
+++ level_1.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
-#a
-#bc
-func _ready() -> void:
-	pass
-#test
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("quit"):
+		get_tree().quit()
+	if Input.is_action_pressed("reset"):
+		get_tree().reload_current_scene()
---
+++ scripts/player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody3D
+
+@export var movement_speed := 8.0
+@export var jump_force := 5.0
+@export var gravity := 10.0
+
+func _physics_process(delta: float) -> void:
+	var movement_direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_forward"):
+		movement_direction.z -= 1
+	if Input.is_action_pressed("move_backward"):
+		movement_direction.z += 1
+	if Input.is_action_pressed("move_left"):
+		movement_direction.x -= 1
+	if Input.is_action_pressed("move_right"):
+		movement_direction.x += 1
+		
+	if is_on_floor():
+		velocity.y = 0
+		if Input.is_action_pressed("jump"):
+			velocity.y = jump_force
+	else:
+		velocity.y -= gravity * delta
+	
+	if movement_direction != Vector3.ZERO:
+		movement_direction = movement_direction.normalized()
+		velocity.z = movement_direction.z * movement_speed
+		velocity.x = movement_direction.x * movement_speed
+	else:
+		velocity.z = 0
+		velocity.x = 0
+	move_and_slide()
