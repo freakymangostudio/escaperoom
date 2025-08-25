3d998c02107a6a16884d949a5784fd0fcdb12db1
Freaky Mango Studio <office@freakymango-studio.com>
2025-08-25 11:47:55 +0100

    24.08.

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,20 +4,25 @@ extends CharacterBody3D
 @export var jump_force := 5.0
 @export var gravity := 10.0
 
+@export var mouse_sense := 0.005
+var pitch := 0.0
+
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
-func _physics_process(delta: float) -> void:
-	var movement_direction = Vector3.ZERO
+func _physics_process(delta: float) -> void: 
+	var input_vec: Vector2 = Input.get_vector("move_left", "move_right", "move_backward", "move_forward")
+
+	var cam_basis = $"Body/Head/Camera3D".global_transform.basis
+	var forward : Vector3 = -cam_basis.z 
+	forward.y = 0 
+	forward = forward.normalized()
+
+	var right : Vector3 = cam_basis.x 
+	right.y = 0 
+	right = right.normalized()
+	var direction = right * input_vec.x + forward * input_vec.y
 	
-	if Input.is_action_pressed("move_forward"):
-		movement_direction.z -= 1
-	if Input.is_action_pressed("move_backward"):
-		movement_direction.z += 1
-	if Input.is_action_pressed("move_left"):
-		movement_direction.x -= 1
-	if Input.is_action_pressed("move_right"):
-		movement_direction.x += 1
 	if Input.is_action_pressed("set_mouse_free"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
@@ -28,16 +33,21 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.y -= gravity * delta
 	
-	if movement_direction != Vector3.ZERO:
-		movement_direction = movement_direction.normalized()
-		velocity.z = movement_direction.z * movement_speed
-		velocity.x = movement_direction.x * movement_speed
-	else:
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		velocity.z = direction.z * movement_speed
+		velocity.x = direction.x * movement_speed
+	else:	
 		velocity.z = 0
 		velocity.x = 0
 	move_and_slide()
 	
 func _input(event: InputEvent) -> void:
-	pass
-	
-	
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sense)
+		
+		pitch -= event.relative.y * mouse_sense
+		pitch = clamp(pitch, deg_to_rad(-80), deg_to_rad(80))
+		
+		$"Body/Head".rotation.x = pitch
+		
